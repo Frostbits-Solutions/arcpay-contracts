61d3368f99b04887e1009887ee0019b1384b8783
kilagg <audranthomas69@gmail.com>
2024-07-07 23:06:46 +0200

    update dutch arc200 arc72 with fees

--- dutch_arc200_arc72.py
+++ dutch_arc200_arc72.py
@@ -1,8 +1,8 @@
 from pyteal import *
-from subroutine import FEES_ADDRESS, PURCHASE_FEES
-from subroutine import nft_id, nft_app_id, fees_address, arc200_app_id, arc200_app_address
-from subroutine import function_transfer_arc72, function_fund_arc200, function_send_note, function_transfer_arc200
-from subroutine import on_fund, on_delete, nft_max_price, nft_min_price, start_time_key, end_time_key
+from subroutine import FEES_ADDRESS, FEES_APP_ID, NOTE_ADDRESS, function_contract_fees_arc200, function_close_app
+from subroutine import nft_id, nft_app_id, fees_address, arc200_app_id, arc200_app_address, counter_party_address, counter_party_fees
+from subroutine import function_transfer_arc72, function_fund_arc200, function_send_note, function_transfer_arc200, main_fees, fees_app_id
+from subroutine import on_fund, on_delete, nft_max_price, nft_min_price, start_time_key, end_time_key, completion_reject, note_address
 
 
 def approval_program():
@@ -15,10 +15,15 @@ def approval_program():
         App.globalPut(end_time_key, Btoi(Txn.application_args[4])),
         App.globalPut(arc200_app_id, Btoi(Txn.application_args[5])),
         App.globalPut(arc200_app_address, Txn.application_args[6]),
+        App.globalPut(counter_party_address, Txn.application_args[7]),
+        App.globalPut(counter_party_fees, Btoi(Txn.application_args[8])),
         App.globalPut(start_time_key, Global.latest_timestamp()),
         App.globalPut(fees_address, Addr(FEES_ADDRESS)),
         Assert(App.globalGet(nft_max_price) > App.globalGet(nft_min_price)),
         Assert(App.globalGet(end_time_key) > App.globalGet(start_time_key)),
+        App.globalPut(main_fees, Int(2)),
+        App.globalPut(fees_app_id, Int(FEES_APP_ID)),
+        App.globalPut(note_address, Addr(NOTE_ADDRESS)),
         Approve(),
     )
 
@@ -50,10 +55,45 @@ def approval_program():
             )
         ),
         Seq(
+            function_contract_fees_arc200(
+                Div(
+                    Mul(
+                        Btoi(Txn.application_args[1]),
+                        Add(
+                            App.globalGet(main_fees),
+                            App.globalGet(counter_party_fees)
+                        )
+                    ),
+                    Int(100)
+                ),
+                Div(
+                    Mul(
+                        Btoi(Txn.application_args[1]),
+                        App.globalGet(counter_party_fees)
+                    ),
+                    Int(100)
+                )
+            ),
             function_fund_arc200(),
-            function_transfer_arc200(Btoi(Txn.application_args[1])-Int(PURCHASE_FEES), Global.creator_address()),
+            function_transfer_arc200(
+                Minus(
+                    Btoi(Txn.application_args[1]),
+                    Div(
+                        Mul(
+                            Btoi(Txn.application_args[1]),
+                            Add(
+                                App.globalGet(main_fees),
+                                App.globalGet(counter_party_fees)
+                            )
+                        ),
+                        Int(100)
+                    )
+                ),
+                Global.creator_address()
+            ),
             function_transfer_arc72(Txn.sender()),
-            function_send_note(Int(PURCHASE_FEES), Bytes("dutch,buy,200/72")),
+            function_send_note(Int(0), Bytes("dutch,buy,200/72")),
+            function_close_app(),
             Approve()
         ),
         Reject()
@@ -65,13 +105,6 @@ def approval_program():
         [And(Txn.on_completion() == OnComplete.NoOp, Txn.application_args[0] == Bytes("fund")), on_fund("dutch,create,200/72")],
         [And(Txn.on_completion() == OnComplete.NoOp, Txn.application_args[0] == Bytes("pre_validate")), Approve()],
         [And(Txn.on_completion() == OnComplete.NoOp, Txn.application_args[0] == Bytes("buy")), on_buy],
-        [
-            Or(
-                Txn.on_completion() == OnComplete.OptIn,
-                Txn.on_completion() == OnComplete.CloseOut,
-                Txn.on_completion() == OnComplete.UpdateApplication
-            ),
-            Reject(),
-        ],
+        completion_reject()
     )
     return program
